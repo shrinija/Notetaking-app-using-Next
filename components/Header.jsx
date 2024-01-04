3f432bb3151754b60ad7f6197c4c81ce077dee7d
shrinija <2020bcs034@sggs.ac.in>
2024-01-04 17:56:17 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header=()=>{
+  return (
+    <header>
+      <h1 className="">Keeper</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Note.jsx
@@ -0,0 +1,11 @@
+
+const Note=()=>{
+  return(
+    <div className="note">
+      <h1>This is the title</h1>
+      <p>This is the heading</p>
+    </div>
+  )
+};
+
+export default Note;
