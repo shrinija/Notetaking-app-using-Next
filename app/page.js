9dca13adc0fab9bf09de403d2f42501489689674
shrinija <2020bcs034@sggs.ac.in>
2024-01-04 19:10:36 +0530

    part done with mapping notes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Note from "@/components/Note";
+import notes from "/public/notes";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Note />
+
+      {notes.map(newnote => (
+        <Note
+          key={newnote.key}
+          title={newnote.title}
+          content={newnote.content}
+        />
+      ))}
+
       <Footer />
     </div>
   );
