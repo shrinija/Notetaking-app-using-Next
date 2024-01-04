c3039453cb2bbc28b730369449318416265dabc1
shrinija <2020bcs034@sggs.ac.in>
2024-01-04 18:41:27 +0530

    part 2 of mapping each note

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Note from "@/components/Note";
+import notes from "/public/Note";
+
+
+function passnote(newnote) {
+  return(
+    <Note
+    key={newnote.key}
+    title={newnote.title}
+    content={newnote.content}
+    />
+  )
+}
 
 const Home = () => {
+
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(passnote)}
       <Footer />
     </div>
   );
